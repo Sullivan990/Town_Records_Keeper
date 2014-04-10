2868c2a2f7abc7398295d8e00af1f01ece481879
Sullivan990 <sullivan99o@yahoo.com>
2014-04-10 15:22:52 -0400

    Initial commit.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+TownRecords::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TownRecords::Application.config.secret_key_base = 'ec0487d6caee082ad0415f9fa2f749a6ed53379be1c790e4444739c26ba0f6ace01616c6ede6656daa8f582f779de4d38874f0b042a3ed35747e50189b81a07c'
