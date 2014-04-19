b2976a87154a7a99b3b265d8940bb456d73e69a8
Sullivan990 <sullivan99o@yahoo.com>
2014-04-19 15:21:02 -0400

    Index and links added.

--- app/controllers/birth_records_controller.rb
+++ app/controllers/birth_records_controller.rb
@@ -1,17 +1,21 @@
 class BirthRecordsController < ApplicationController
   def new
-    @birth_record = BirthRecord.new
+    @birthrecord = BirthRecord.new
   end
 
   def create
-    @birth_record = BirthRecord.new(params.require(:birth_record)
+    @birthrecord = BirthRecord.new(params.require(:birth_record)
         .permit(:birth_month, :birth_year, :birth_day, :first_name,
          :middle_name, :second_name, :last_name, :name_suffix, :code))
 
-    if @birth_record.save
+    if @birthrecord.save
       redirect_to new_birth_record_path, notice: 'Record successfully added.'
     else
       render :new
     end
+  end
+
+  def index
+    @birthrecord = BirthRecord.all
   end
 end
